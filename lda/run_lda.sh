57148fe19350f765256cd45873fbd98c7cd850ac
Michael Greenberg <michael.m.greenberg@gmail.com>
2015-01-02 09:59:34 -0500

    dating of different runs

--- lda/run_lda.sh
+++ lda/run_lda.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-lda est 1/50 50 settings.txt abstracts.dat seeded lda50 &
-lda est 1/50 75 settings.txt abstracts.dat seeded lda75 &
-lda est 1/50 100 settings.txt abstracts.dat seeded lda100 &
-lda est 1/50 125 settings.txt abstracts.dat seeded lda125 &
-lda est 1/50 150 settings.txt abstracts.dat seeded lda150 &
-lda est 1/50 175 settings.txt abstracts.dat seeded lda175 &
-lda est 1/50 200 settings.txt abstracts.dat seeded lda200 &
+PREFIX=`date "+%Y-%m-%d_%H:%M"`
+
+lda est 1/50  50 settings.txt abstracts.dat seeded ${PREFIX}_lda50 &
+lda est 1/50  75 settings.txt abstracts.dat seeded ${PREFIX}_lda75 &
+lda est 1/50 100 settings.txt abstracts.dat seeded ${PREFIX}_lda100 &
+lda est 1/50 125 settings.txt abstracts.dat seeded ${PREFIX}_lda125 &
+lda est 1/50 150 settings.txt abstracts.dat seeded ${PREFIX}_lda150 &
+lda est 1/50 175 settings.txt abstracts.dat seeded ${PREFIX}_lda175 &
+lda est 1/50 200 settings.txt abstracts.dat seeded ${PREFIX}_lda200 &
 
 wait
 echo "DONE, outputting topics..."
 
-for i in lda*; do
+for i in ${PREFIX}_lda*; do
     python topics.py ${i}/final.beta vocab.dat 15 > ${i}_topics.txt
 done
 
