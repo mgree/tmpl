dbb0c1d045233a531ea364490d57a9884b2f8135
Michael Greenberg <michael.m.greenberg@gmail.com>
2015-03-12 12:24:39 -0400

    starting on the frontend

---
+++ www/frontend/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>TMPL: Topic Modeling in PL</title>
+
+</head>
+<body>
+<center>
+  <h1>TMPL : Topic Modeling in PL</h1>
+  <h2>Tracking the flow of ideas through the programming languages literature</h2>
+</center>
+</body>
+</html>
