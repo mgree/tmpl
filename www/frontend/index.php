3cbd5b3b84e55933a2b3f20c5004b55b6b1cbb9b
Michael Greenberg <michael.m.greenberg@gmail.com>
2015-03-12 12:39:44 -0400

    OWA tracking, author list

--- www/frontend/index.php
+++ www/frontend/index.php
@@ -1,12 +1,26 @@
 <html>
 <head>
 <title>TMPL: Topic Modeling in PL</title>
+<?
 
+require_once('../../owa/owa_php.php');
+
+$owa = new owa_php();
+$owa->setSiteId('101db06411ab85d334726f4ec2344077');
+$owa->setPageTitle('index.php');
+$owa->trackPageView();
+
+?>
 </head>
 <body>
 <center>
   <h1>TMPL : Topic Modeling in PL</h1>
   <h2>Tracking the flow of ideas through the programming languages literature</h2>
+  <h3>
+    <a href="http://www.cs.princeton.edu/~mg19/">Michael Greenberg</a>,
+    <a href="http://www.cs.tufts.edu/~kfisher/Kathleen_Fisher/Home.html">Kathleen Fisher</a>, and
+    <a href="http://www.cs.princeton.edu/~dpw/">David Walker</a>
+  </h3>
 </center>
 </body>
 </html>
