d0c1681d4669cb1c3670bfa09b4a5061d9a9cbde
Michael Greenberg <michael.m.greenberg@gmail.com>
2015-09-15 11:06:05 -0700

    ack noam

--- analysis/topic_weights/topic_histogram.r
+++ analysis/topic_weights/topic_histogram.r
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(reshape2)
+library(dplyr) # run install.packages('dplyr') to get this
 
 topic_names <- read.csv("../topic_names/final_lda20_fulltext.txt",header=FALSE,as.is=TRUE)
 
@@ -11,5 +12,9 @@ names(lda)[4:23] <- topic_names[1:20,]
 d <- melt(lda,seq(1,3))
 names(d)[4:5] <- c("Topic","Weight")
 
-p <- ggplot(d, aes(x=Topic,y=Weight,fill=Conference))
-p + geom_bar(stat="identity", position="dodge") + theme(strip.text=element_text(size=10), axis.text.x=element_text(angle=90)) 
+# thanks to Noam Ross for the tip no using dplyr/this bit of code
+d2 = d %>% group_by(Topic, Conference) %>% summarize(Total = sum(Weight))
+
+p <- ggplot(d2, aes(x=Topic,y=Total,fill=Conference))
+p + geom_bar(stat="identity") + theme(strip.text=element_text(size=10), 
+                                      axis.text.x=element_text(angle=90, hjust=1)) 
