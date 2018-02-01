912e67f2fb942504f182bb2dcab30c2cb3bd9ad6
Sean MacPherson <seansdevelopmentemail@gmail.com>
2018-01-31 20:50:24 -0800

    Added more questions to Questions.md; realized list comprehension destructively modifies lists

--- experimental/Questions.md
+++ experimental/Questions.md
@@ -2,5 +2,7 @@
 
 How are we tokenizing the documents? By words? What punctuation, apostrophes (I guess those are interpreted as one word), and hyphenated terms (I did see something in the parse.py about replacing hyphens)?
 
-Should I use stemming or lemmatization? Have we experimented with the performance / effects of both yet? I saw a `use_wordnet` flag in the parse.py file but wasn't sure if we ever set that to `False`.
+Should I use stemming or lemmatization? Have we experimented with the performance / effects of both yet? I saw a `use_wordnet` flag in the parse.py file but wasn't sure if we ever set that to `False`. It seems like stemming and lemmatization would have a pretty significant effect on the performance of a topic model given that we want the topic model to be able to pick up on similar / related words.
+
+Should I be using list comprehension to process lists of documents in place (is there a memory constraint)? Or can I non-destructively preprocess lists of documents? It might be useful to do the latter to cached intermediate results just for development purposes so that if we're testing a specific part of the pipeline, we don't have to preprocess the entire corpus each time.
 
--- experimental/test.py
+++ experimental/test.py
@@ -3,6 +3,8 @@ import logging
 from pprint import pprint
 from gensim import corpora
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
 
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', 
@@ -36,15 +38,40 @@ stoplist = set(stopwords.words('english'))
 """ 1. Pre-process corpus. """
 
 def preprocess(documents):
+    """Cleans, tokenizes, and stems a list of plain string documents.
+    
+    Args:
+        documents: list of plain string documents to be preprocessed.
+
+    Returns:
+        A list of cleaned and stemmed token lists. 
+    """
+    logging.info('Raw documents...')
+    logging.info(documents)
+
+    logging.info('Cleaning...')
     cleaned = applyRules(documents, RULES)
+    logging.info(cleaned)
+
+    logging.info('Tokenizing...')
     tokenized = tokenizeDocuments(cleaned)
-    return tokenized
+    logging.info(tokenized)
+
+    logging.info('Stemming...')
+    stemmed = stemTokenLists(tokenized)
+    logging.info(stemmed)
+
+    logging.info('Lemmatizing...')
+    lemmatized = lemmatizeTokenList(tokenized)
+    logging.info(lemmatized)
+
+    return stemmed
 
 
 # 1.a. Tokenization: convert document to individual elements.
 # 1.b. Stopword removal: remove trivial, meaningless words.
 def tokenizeDocuments(documents):
-    """Tokenizes documents.
+    """Tokenizes a list of documents.
 
     Args:
         document: list of plain string documents to tokenize.
@@ -56,7 +83,7 @@ def tokenizeDocuments(documents):
 
 
 def tokenizeDocument(document):
-    """Converts document to lowercase, removes stopwords, and tokenizes.
+    """Converts a single document to lowercase, removes stopwords, and tokenizes.
 
     Args:
         document: plain string document to be tokenized.
@@ -87,13 +114,72 @@ def applyRules(documents, rules):
         result = [document.replace(old, new) for document in documents]
     return result
 
-# 1.c. Stemming / Lemmatizing: collapse words with equivalent semantics.
+# 1.c. Stemming / Lemmatizing: collapse words with similar semantics 
+# but different syntactic forms.
 
+# 1.c. (Option 1: Stemming)
+# Chops off the ends of the words using a rough heuristic process that
+# takes into account the length of the word, number of syllables, etc. to 
+# chop off the end of a word in the hopes are arriving at its atomic stem.
+def stemTokenLists(tokenLists):
+    """Stems a list of token lists.
 
+    Args:
+        tokenLists: list of token lists to be stemmed.
+
+    Returns:
+        A list of stemmed token lists.
+    """
+    result = None
+    for tokenList in tokenLists:
+        result.append(stemTokenLists)
+    return [stemTokenList(tokenList) for tokenList in tokenLists]
+
+def stemTokenList(tokenList):
+    """Stems a single token list. Trims off ends of words in
+    the hopes of reducing semantically related words to the same stem.
+
+    Eg. 'chopping', 'chopped', 'chopper' --> 'chop'
+
+    Args:
+        tokenLists: the single token list to be stemmed.
+
+    Returns:
+        A stemmed token lists.
+    """
+    stemmer = PorterStemmer()
+    return [stemmer.stem(token) for token in tokenList]
+
+# 1.c. (Option 2: Lemmatization)
+# Uses vocubulary and morphological analysis with the aim of
+# only removing inflectional endings (eg. 'chopping' vs. 'chopped')
+# and returning the dictionary base of a word -- known as the word's 'lemma.'
+def lemmatizeTokenLists(tokenLists):
+    """Lemmatizes a list of token lists.
+
+    Args:
+        tokenLists: list of token lists to be lemmatized.
+
+    Returns:
+        A list of lemmatized token lists.
+    """
+    return [lemmatizeTokenList(tokenList) for tokenList in tokenLists]
+
+def lemmatizeTokenList(tokenList):
+    """Lemmatizes a single token list.
+
+    Args:
+        tokenList: the single token list to be lemmatized.
+
+    Returns:
+        A lemmatized token list.
+    """
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(token) for token in tokenList]
 
 def main():
     preprocessed = preprocess(documents)
-    logging.info(preprocessed)
+
 
 if __name__ == '__main__':
     print("Stop words:")
